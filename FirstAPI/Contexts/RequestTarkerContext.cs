f95ac0eef81ddc9e1905f7d78b07ba0451f8b412
Your Name <you@example.com>
2024-01-29 16:50:02 +0530

    Initial

---
+++ FirstAPI/Contexts/RequestTarkerContext.cs
@@ -0,0 +1,18 @@
+﻿using FirstAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstAPI.Contexts
+{
+    public class RequestTarkerContext : DbContext
+    {
+        public RequestTarkerContext()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data source=DESKTOP-9L5BOE7;Integrated Security=true;Initial catalog=dbRequestTraker");
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
